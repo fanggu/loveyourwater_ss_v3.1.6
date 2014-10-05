8d90321cb15e4ab81ac3647676076290dc44d728
fang@viia.co.nz <>
2014-10-05 22:35:18 +1300

    Add event_calendar module

---
+++ event_calendar/code/CalendarAnnouncement.php
@@ -0,0 +1,31 @@
+<?php
+
+class CalendarAnnouncement extends CalendarDateTime {
+
+	private static $db = array (
+		'Title' => 'Varchar(255)',
+		'Content' => 'Text'
+	);
+
+	private static $has_one = array (
+		'Calendar' => 'Calendar'
+	);
+
+	public function getCMSFields() {
+		$f = parent::getCMSFields();
+		$f->insertBefore(new TextField('Title', _t('CalendarAnnouncement.TITLE','Title of announcement')), "StartDate");
+		$f->insertBefore(new TextareaField('Content', _t('CalendarAnnouncement.CONTENT','Announcement content')), "StartDate");		
+		$this->extend('updateCMSFields', $f);
+
+		return $f;
+	}
+
+	public function getTitle() {
+		return $this->getField('Title');
+	}
+
+	public function getContent() {
+		return $this->getField('Content');
+	}
+
+}
